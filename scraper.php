e9b64549bad298d8509803f36a220dc5dbd44e7e
John Handelaar <john@handelaar.org>
2017-07-01 20:59:25 +0100

    Create with notes in comments for later

--- scraper.php
+++ scraper.php
@@ -1,9 +1,14 @@
 <?
-// This is a template for a PHP scraper on morph.io (https://morph.io)
-// including some code snippets below that you should find helpful
+// Get feature ID of planning application
+// http://www.dublincity.ie/LocationPublisher/SearchResult.aspx?Group_ID=1&amp;SearchString=3190/17&amp;IsSearchSP=1&amp;RefreshGuid=9a9919ee-7457-4412-9b43-58e74dcbd3cc
+//
+// Get geometry of feature ID
+// POST to http://www.dublincity.ie/LPA/GetGeoJSONFeatureGeometry.asmx/GetFeatureBySearchInformation
+// POST data {"nSearchID":3,"nFeatureKeyFieldValue":64931145}
+// 
 
-// require 'scraperwiki.php';
-// require 'scraperwiki/simple_html_dom.php';
+require 'scraperwiki.php';
+require 'scraperwiki/simple_html_dom.php';
 //
 // // Read in a page
 // $html = scraperwiki::scrape("http://foo.com");
