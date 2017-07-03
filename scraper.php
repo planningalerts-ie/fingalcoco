6e9c761579841c0bcc9a8fa8a6d1ab51b3032c7c
John Handelaar <john@handelaar.org>
2017-07-03 04:22:07 +0100

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -19,7 +19,7 @@ include_once('vendor/phayes/geophp/geoPHP.inc');
 $test = file_get_contents('http://gis.fingal.ie/arcgis/rest/services/Planning/PlanningApplicationsWeb/MapServer/2/query?f=json&where=PLANNING_REFERENCE%3D%27F17A%2F0314%27&returnGeometry=true&spatialRel=esriSpatialRelIntersects&maxAllowableOffset=0.00001&outFields=*&outSR=4326');
 $application = json_decode($test->features[0]->geometry);
 
-echo $application . "\n\n";
+print_r($application); echo "\n\n";
 
 #$polygon = geoPHP::load(json_encode($application),'json');
 
