4df598416ed0c54542846a9028c203df5b287629
John Handelaar <john@handelaar.org>
2017-07-03 19:46:12 +0100

    Comments added

--- scraper.php
+++ scraper.php
@@ -8,6 +8,13 @@ require 'scraperwiki.php';
 require 'scraperwiki/simple_html_dom.php';
 include_once('vendor/phayes/geophp/geoPHP.inc');
 
+
+/* 
+ * Presently this does nowt but convert a single planning reference from Fingal,
+ * which is represented in its GIS maps as a SRID-2157 ITM Polygon, into a 
+ * WGS84 point of its centroid, then spit out that centroid. More to follow
+ */
+
 $coords = getPointFromJSONURI('F17A/0314');
 
 print_r($coords);
@@ -46,25 +53,4 @@ function makeGeoJson($object) {
   return $geojson;
 }
 
-
-  
-// // Read in a page
-// $html = scraperwiki::scrape("http://foo.com");
-//
-// // Find something on the page using css selectors
-// $dom = new simple_html_dom();
-// $dom->load($html);
-// print_r($dom->find("table.list"));
-//
-// // Write out to the sqlite database using scraperwiki library
-// scraperwiki::save_sqlite(array('name'), array('name' => 'susan', 'occupation' => 'software developer'));
-//
-// // An arbitrary query against the database
-// scraperwiki::select("* from data where 'name'='peter'")
-
-// You don't have to do things with the ScraperWiki library.
-// You can use whatever libraries you want: https://morph.io/documentation/php
-// All that matters is that your final data is written to an SQLite database
-// called "data.sqlite" in the current working directory which has at least a table
-// called "data".
 ?>
